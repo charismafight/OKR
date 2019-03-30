9f0ac422a3d1ab65630847f02f556fc9c8157dd2
Lee <charismafight@hotmail.com>
2019-03-31 02:06:07 +0800

    add index and detail link

--- OKR/OKR/settings.py
+++ OKR/OKR/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'django_tables2',
     'Objective.apps.ObjectiveConfig',
     'django.contrib.admin',
     'django.contrib.auth',
