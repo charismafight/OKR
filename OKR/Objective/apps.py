928e7618ad9426b1fa5a31c94df2a77a549bb0b3
Lee <charismafight@hotmail.com>
2019-04-24 19:58:45 +0800

    add some dxj tag

--- OKR/Objective/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ObjectiveConfig(AppConfig):
-    name = 'Objective'
--- OKR/Objective/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    url('Objective_id/(\d+)/', views.objective_detail, name='key_result_detail'),
-]
